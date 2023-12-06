43e4115a967b410a74297735fcc3fd580d9865c2
nosaboy <67034785+nosaboy@users.noreply.github.com>
2023-12-05 23:24:03 -0700

    Update Meeting 12-5-2023.py

--- Meeting 12-5-2023.py
+++ Meeting 12-5-2023.py
@@ -1,3 +1,39 @@
+# EXAMPLE 1
+n = int(input())
+m = int(input())
+adj = []
+noun = []
+for i in range(n):
+	s = input()
+	adj.append(s)
+for i in range(m):
+	s = input()
+	noun.append(s)
+
+for i in range(n):
+	for j in range(m):
+		print(f"{adj[i]} as {noun[j]}")
+
+# EXAMPLE 2
+n = int(input())
+pepper = {
+	"Poblano":1500,
+	"Mirasol":6000,
+	"Serrano":15500,
+	"Cayenne":40000,
+	"Thai":75000,
+	"Habanero":125000
+
+}
+ans = 0
+for i in range(n):
+	name = input()
+	ans = ans + pepper[name]
+
+print(ans)
+
+# EXAMPLE 3
+
 n = int(input())
 pairing = dict() # empty dict
 s = input().split() # turns line 2 into list of strings
